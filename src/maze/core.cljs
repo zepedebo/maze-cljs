ec1eb10f123ff448d81cfa05c0576e2eeee9cf72
Steve Goodrich <steven.goodrich@landesk.com>
2015-11-29 13:53:14 -0700

    Generating a grid

--- src/maze/core.cljs
+++ src/maze/core.cljs
@@ -7,10 +7,36 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defn genboard []
-  (into-array (repeat 40 (into-array (repeat 40 "blue")))))
 
-(def app-state (atom {:maze (genboard)}))
+(defn setCell [maze row col color]
+  (assoc maze row
+    (let [newRow (nth maze row)]
+      (assoc newRow col color))))
+
+
+
+(defn hline [maze start end row color]
+  (loop [acc maze
+         col start]
+    (if (> col end) acc
+        (recur (setCell acc row col color) (+ col 1)))))
+
+(defn vline [maze start end col color]
+  (loop [acc maze
+         row start]
+    (if (> row end) acc
+        (recur (setCell acc row col color) (+ row 1))
+
+(defn genrow [width]
+  (vec (replicate width "blue")))
+
+(defn genboard [width height]
+  (let [a (vec (replicate height (genrow width)))]
+    (loop [acc a pos 0]
+      (if (> pos 39) acc
+          (recur (-> acc (hline 0 39 pos "red") (vline 0 39 pos "red")) (+ 3 pos))))))
+
+(def app-state (atom {:maze (genboard 40 40)}))
 
 (defn drawCell [row col color]
   [:rect {:x col :y row :width 10 :height 10 :style {:fill color}}])
@@ -30,6 +56,10 @@
           rownum 0]
         (if (empty? board) acc
             (recur (concat acc (drawRow (first board) rownum)) (rest board) (+ rownum 10)))))
+
+(defn genMaze
+  (let [board (:maze @app-state)]
+    ()))
 
 
 (defn hello-world []
